d2c04aa62b0f007a9dc949be55fa2e1bb161eb48
PauloStraforini <paulostraforini@gmail.com>
2025-10-27 14:26:39 -0300

    feat(db): configure drizzle

--- api/src/env.ts
+++ api/src/env.ts
@@ -1,8 +1,11 @@
-import { z } from "zod"
+import { z } from 'zod'
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
-    PORT: z.coerce.number().default(3333),
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.url(),
 })
 
-export const env  = envSchema.parse(process.env)
+export const env = envSchema.parse(process.env)
