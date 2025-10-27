27e8d699036178ff6c690a3c92786790928ad602
PauloStraforini <paulostraforini@gmail.com>
2025-10-27 13:50:47 -0300

    chore: configure .env

--- api/src/server.ts
+++ api/src/server.ts
@@ -9,6 +9,7 @@ import { fastifySwagger } from '@fastify/swagger'
 import { fastifyCors } from '@fastify/cors'
 import ScalarApiReference from '@scalar/fastify-api-reference'
 import { listWebhooks } from './routes/list-webhooks'
+import { env } from './env'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -37,7 +38,7 @@ app.register(ScalarApiReference, {
 
 app.register(listWebhooks)
 
-app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running on http://localhost:3333')
   console.log('Docs available at http://localhost:3333/docs')
 })
